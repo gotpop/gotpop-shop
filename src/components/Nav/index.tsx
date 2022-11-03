0f58f39615dfe929f127953606cd3d9007547232
gotpop <liam@gotpop.net>
2022-11-03 18:33:53 +0000

    Adding link

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react'
-import { TfiGithub } from 'react-icons/tfi'
-import { GoZap } from 'react-icons/go'
-import { AiFillHome } from 'react-icons/ai'
+import Link from 'next/link'
 import styles from './Nav.module.css'
+
+import { AiFillHome } from 'react-icons/ai'
+import { BsPeopleFill } from 'react-icons/bs'
+import { GoZap } from 'react-icons/go'
+import { TfiGithub } from 'react-icons/tfi'
 
 export default function Nav() {
   return (
     <nav className={styles.nav}>
+      <Link href="/people">
+        <span>People</span>
+        <BsPeopleFill />
+      </Link>
       <a href="http://localhost:3000">
         <span>LocalHost</span>
         <AiFillHome />
