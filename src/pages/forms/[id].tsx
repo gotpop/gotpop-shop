196e22d499bebe8b18f3401d8e96594ff7cc4b88
gotpop <liam@gotpop.net>
2022-11-04 14:34:21 +0000

    Adding forms pages

---
+++ src/pages/forms/[id].tsx
@@ -0,0 +1,31 @@
+import Card from '@components/Card'
+import Layout from '@components/layout/layout'
+import Loading from '@components/Loading'
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
+
+const fetcher = async (url: string) => {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  if (res.status !== 200) {
+    throw new Error(data.message)
+  }
+  return data
+}
+
+export default function CardPage() {
+  const { query } = useRouter()
+  const { data: cards, error } = useSWR(
+    () => query.id && `/api/cards/${query.id}`,
+    fetcher
+  )
+
+  return (
+    <Layout>
+      {error && <div>{error.message}</div>}
+      {!cards && <Loading />}
+      {cards && <Card content={cards} fullCard={true} />}
+    </Layout>
+  )
+}
---
+++ src/pages/forms/index.tsx
@@ -0,0 +1,21 @@
+import useSWR from 'swr'
+import { Card as CardType } from 'types'
+import Layout from '@components/layout/layout'
+import Card from '@components/Card'
+import Loading from '@components/Loading'
+
+const fetcher = (url: string) => fetch(url).then(res => res.json())
+
+export default function Cards() {
+  const { data: cards, error } = useSWR('/api/cards', fetcher)
+
+  return (
+    <Layout>
+      {error && <div>Failed to load</div>}
+      {!cards && <Loading />}
+      {cards?.map((card: CardType) => (
+        <Card key={card.id} content={card} fullCard={false} />
+      ))}
+    </Layout>
+  )
+}
