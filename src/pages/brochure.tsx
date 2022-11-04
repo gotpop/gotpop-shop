b223d596b7ba174e13a14c8408527d3f8dd8e00f
gotpop <liam@gotpop.net>
2022-11-04 17:19:31 +0000

    Updating brochure

--- src/pages/brochure.tsx
+++ src/pages/brochure.tsx
@@ -1,13 +1,7 @@
 import useSWR from 'swr'
-import { Card as CardType } from 'types'
 import Layout from '@components/Layout'
-import Card from '@components/Card'
-import Loading from '@components/Loading'
 
-const fetcher = (url: string) => fetch(url).then(res => res.json())
-
-export default function Cards() {
-  const { data: cards, error } = useSWR('/api/cards', fetcher)
+export default function Brochure() {
 
   return (
     <Layout>
