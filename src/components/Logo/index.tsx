5117cfdb79ee816b1079a563c569689aa12a3f6a
gotpop <liam@gotpop.net>
2022-11-04 02:38:25 +0000

    Adding icon logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import styles from './Logo.module.css'
+import { GiSpiralLollipop } from 'react-icons/gi'
 
 export default function Logo() {
   return (
     <Link className={styles.logo} href="/">
-      <h1>GotPop Starter</h1>
+      <h1><GiSpiralLollipop /><span>Starter</span></h1>
     </Link>
   )
 }
