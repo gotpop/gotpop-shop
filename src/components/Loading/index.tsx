d7a0476641698c54fc76dc6336c355e865b40c8a
gotpop <liam@gotpop.net>
2022-11-04 02:30:39 +0000

    Adding css loader

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,7 +3,7 @@ import styles from './Loading.module.css'
 export default function Loading() {
   return (
     <div className={styles.loading}>
-      <div className={styles.ldsroller}>
+      <div className={styles.spinner}>
         <div></div>
         <div></div>
         <div></div>
