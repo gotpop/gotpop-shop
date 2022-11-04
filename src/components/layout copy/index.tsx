5bf5d333659e59acd08420ebc5f935aade31ad14
gotpop <liam@gotpop.net>
2022-11-04 20:16:06 +0000

    File path test capital

---
+++ src/components/layout copy/index.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+import Trigger from '@components/Trigger'
+import styles from './Site.module.css'
+import stylesMain from './Main.module.css'
+import { getScrollBarWidth } from '@utils/scrollBarWidth'
+import { useEffect } from 'react'
+
+export default function Layout({ children }) {
+  useEffect(() => {
+    getScrollBarWidth()
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>CSS 2023</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+      </Head>
+      <div className={styles.site}>
+        <Header />
+        <div className={stylesMain.mainwrap}>
+          <main className={stylesMain.main}>
+            <Trigger />
+            {children}
+          </main>
+        </div>
+        <Footer />
+      </div>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import Layout from '@components/layout'
+import Layout from '@components/Layout'
 import Intro from '@components/Intro'
 import { introContent } from '@content/intro'
 
