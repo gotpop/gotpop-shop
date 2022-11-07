1db03bf2546fc0de45bf26db7dfde99902d802fe
gotpop <liam@gotpop.net>
2022-11-07 20:35:08 +0000

    Updating form endpoint

--- src/pages/api/forms/form.ts
+++ src/pages/api/forms/form.ts
@@ -1,20 +1,15 @@
 export default function handler(req, res) {
-    // Get data submitted in request's body.
-    const body = req.body
-  
-    // Optional logging to see the responses
-    // in the command line where next.js app is running.
-    console.log('body: ', body)
-  
-    // Guard clause checks for first and last name,
-    // and returns early if they are not found
-    if (!body.firstName || !body.lastName ) {
-      // Sends a HTTP bad request error code
-      return res.status(400).json({ data: 'First or last name not found' })
-    }
-  
-    // Found the name.
-    // Sends a HTTP success code
-    res.status(200).json({ data: `${body.firstName.value} ${body.lastName.value}` })
+  const body = req.body
+
+  console.log('body: ', body)
+
+
+  if (!body.firstName || !body.lastName || !body.email || !body.password) {
+    return res.status(400).json({ data: 'First or last name not found' })
   }
-  
+
+  res.status(200).json({
+    firstName: body.firstName.value,
+    lastName: body.lastName.value
+  })
+}
