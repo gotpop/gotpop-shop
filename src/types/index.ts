d923a9c62053885881c23b96d143d787648e1b18
gotpop <liam@gotpop.net>
2022-11-09 13:32:26 +0000

    Types formatting

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,41 +2,42 @@ import { MouseEventHandler } from "react";
 import { IconType } from "react-icons";
 
 export interface IButtonIcon {
-    content: string
-    properties?: { local: string; global: string }[]
-    icon?: IconType
-    doClick?: MouseEventHandler<HTMLButtonElement>
+    content: string;
+    properties?: { local: string; global: string }[];
+    icon?: IconType;
+    doClick?: MouseEventHandler<HTMLButtonElement>;
+    disabled?: boolean;
 }
 
 export interface ILinkIcon {
-    content?: string
-    properties?: { local: string; global: string }[]
-    icon?: IconType
-    href: string
+    content?: string;
+    properties?: { local: string; global: string }[];
+    icon?: IconType;
+    href: string;
 }
 
 export interface Card {
-    id: string
-    text: string
-    title: string
-    links: Link[]
+    id: string;
+    text: string;
+    title: string;
+    links: Link[];
 }
 
 export interface Link {
-    name: string
-    href: string
+    name: string;
+    href: string;
 }
 
 export interface IPanel {
-    id: string
-    text: string
-    title: string
+    id: string;
+    text: string;
+    title: string;
 }
 
 export interface IForm {
-    id: string
-    text: string
-    title: string
+    id: string;
+    text: string;
+    title: string;
 }
 
 export interface IFormProps {
@@ -52,6 +53,6 @@ export interface IForm2 {
 }
 
 export type User = {
-    id: number
-    name?: string
+    id: number;
+    name?: string;
 }
