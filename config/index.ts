67f63aef9419f94d6592508e142bc5627a08cad1
gotpop <liam@gotpop.net>
2022-11-10 19:51:14 +0000

    Determine ENV

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production'
-const local = 'http://localhost:3000'
-const prod = 'https://gotpop-starter.vercel.app'
+const local = process.env.HOST
+const prod = process.env.PRODUCTION
 
 export const server = dev ? local : prod
