dfaf4ede794f74b98ea8de2fc5e993815b5f9405
gotpop <liam@gotpop.net>
2022-11-10 17:11:26 +0000

    Removing SWR in favour of GSP & GSP

--- src/pages/cards/[id].tsx
+++ src/pages/cards/[id].tsx
@@ -1,31 +1,55 @@
 import Card from '@components/Card'
 import LayoutStandard from '@components/LayoutStandard'
 import Loading from '@components/Loading'
+import { server } from '@config'
 import { useRouter } from 'next/router'
-import useSWR from 'swr'
 
-const fetcher = async (url: string) => {
-  const res = await fetch(url)
-  const data = await res.json()
+// import useSWR from 'swr'
 
-  if (res.status !== 200) {
-    throw new Error(data.message)
-  }
-  return data
-}
+// const fetcher = async (url: string) => {
+//   const res = await fetch(url)
+//   const data = await res.json()
 
-export default function CardPage() {
-  const { query } = useRouter()
-  const { data: cards, error } = useSWR(
-    () => query.id && `/api/cards/${query.id}`,
-    fetcher
-  )
+//   if (res.status !== 200) {
+//     throw new Error(data.message)
+//   }
+//   return data
+// }
+
+export default function CardPage({ card }) {
+  // const { query } = useRouter()
+  // const { data: cards, error } = useSWR(
+  //   () => query.id && `/api/cards/${query.id}`,
+  //   fetcher
+  // )
 
   return (
     <LayoutStandard>
-      {error && <div>{error.message}</div>}
-      {!cards && <Loading />}
-      {cards && <Card content={cards} fullCard={true} />}
+      {card && <Card content={card} fullCard={true} />}
     </LayoutStandard>
   )
+}
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(`${server}/api/cards/${context.params.id}`)
+  const card = await res.json()
+
+  return {
+    props: {
+      card
+    }
+  }
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`${server}/api/cards`)
+  const cards = await res.json()
+
+  const ids = cards.map((card) => card.id)
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+
+  return {
+    paths,
+    fallback: false,
+  }
 }
