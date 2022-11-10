fd2e78c12f9328e4f203cb2650cf8d65623e3816
gotpop <liam@gotpop.net>
2022-11-10 16:58:39 +0000

    Removing SWR in favour of GSP

--- src/pages/cards/index.tsx
+++ src/pages/cards/index.tsx
@@ -1,12 +1,12 @@
 import Card from '@components/Card'
-import { Card as CardType } from '@types'
+import { Card as ICard } from '@types'
 import LayoutStandard from '@components/LayoutStandard'
 import { server } from '@config'
 
 export default function Cards({ cards }) {
   return (
     <LayoutStandard>
-      {cards.map((card: CardType) => (
+      {cards.map((card: ICard) => (
         <Card key={card.id} content={card} fullCard={false} />
       ))}
     </LayoutStandard>
