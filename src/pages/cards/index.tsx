9cd825c993dabb2885697355e29636c6af47de34
gotpop <liam@gotpop.net>
2022-11-10 16:56:47 +0000

    Removing SWR in favour of GSP

--- src/pages/cards/index.tsx
+++ src/pages/cards/index.tsx
@@ -1,15 +1,9 @@
 import Card from '@components/Card'
 import { Card as CardType } from '@types'
 import LayoutStandard from '@components/LayoutStandard'
-import Loading from '@components/Loading'
 import { server } from '@config'
-// import useSWR from 'swr'
-
-// const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 export default function Cards({ cards }) {
-  // const { data: cards, error } = useSWR('', fetcher)
-
   return (
     <LayoutStandard>
       {cards.map((card: CardType) => (
