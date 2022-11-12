1fd4f2dd6acee17e5ccc0d2b48b6ddda8360f72f
gotpop <liam@gotpop.net>
2022-11-12 18:52:03 +0000

    Adding get static props & paths

--- src/pages/forms/[id].tsx
+++ src/pages/forms/[id].tsx
@@ -1,3 +1,4 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { introForm1Content, introForm2Content } from '@data/intro'
 
 import Form1 from '@components/Form1'
@@ -6,30 +7,15 @@ import Intro from '@components/Intro'
 import LayoutStandard from '@components/LayoutStandard'
 import Loading from '@components/Loading'
 import Meta from '@components/Meta'
+import { server } from '@config'
 import { useRouter } from 'next/router'
-import useSWR from 'swr'
 
-const fetcher = async (url: string) => {
-  const res = await fetch(url)
-  const data = await res.json()
-
-  if (res.status !== 200) {
-    throw new Error(data.message)
-  }
-  return data
-}
-
-export default function FormPage() {
+export default function FormPage({ form }) {
   const { query } = useRouter()
-  const { data: form, error } = useSWR(
-    () => query.id && `/api/forms/${query.id}`,
-    fetcher
-  )
 
   return (
     <LayoutStandard>
       <Meta />
-      {error && <div>{error.message}</div>}
       {form ? (
         <>
           {query.id === '1' ? (
@@ -49,4 +35,28 @@ export default function FormPage() {
       )}
     </LayoutStandard>
   )
+}
+
+export const getStaticProps: GetStaticProps = async context => {
+  const res = await fetch(`${server}/api/forms/${context.params.id}`)
+  const form = await res.json()
+
+  return {
+    props: {
+      form
+    }
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await fetch(`${server}/api/forms`)
+  const forms = await res.json()
+
+  const ids = forms.map(form => form.id)
+  const paths = ids.map(id => ({ params: { id: id.toString() } }))
+
+  return {
+    paths,
+    fallback: false
+  }
 }
