22e11e65917712f70e3b48a2d7e3af106ad3dc36
gotpop <liam@gotpop.net>
2022-11-12 18:40:00 +0000

    Adding get static props

--- src/pages/forms/index.tsx
+++ src/pages/forms/index.tsx
@@ -4,25 +4,24 @@ import LayoutStandard from '@components/LayoutStandard'
 import Loading from '@components/Loading'
 import Meta from '@components/Meta'
 import { server } from '@config'
-import useSWR from 'swr'
-
-// const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 export default function Forms({ forms }) {
-  // const { data: forms, error } = useSWR('/api/forms', fetcher)
 
   return (
     <LayoutStandard>
       <Meta />
-      {!forms && <Loading />}
-      {forms?.map((form: IForm) => (
-        <Card
-          key={form.id}
-          content={form}
-          fullCard={false}
-          childPath={'forms'}
-        />
-      ))}
+      {forms ? (
+        forms.map((form: IForm) => (
+          <Card
+            key={form.id}
+            content={form}
+            fullCard={false}
+            childPath={'forms'}
+          />
+        ))
+      ) : (
+        <Loading />
+      )}
     </LayoutStandard>
   )
 }
