e766f8371dad8123a4c46391e8697b30c55dd267
gotpop <liam@gotpop.net>
2022-11-13 19:06:41 +0000

    Adding font settings

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
     reactStrictMode: true,
+    experimental: {
+        fontLoaders: [
+            { loader: '@next/font/google', options: { subsets: ['latin'] } },
+        ],
+    },
 }
