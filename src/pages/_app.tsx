92c956d5256933c2a2c717e1e9df120370f80309
gotpop <liam@gotpop.net>
2022-11-13 17:10:41 +0000

    Adding next font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,22 @@
 import '../styles/app.css'
 
 import { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
 import { MenuProvider } from '@context/MenuContext'
+
+const inter = Inter()
 
 export default function StarterApp({ Component, pageProps }: AppProps) {
   return (
-    <MenuProvider>
-      <Component {...pageProps} />
-    </MenuProvider>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <MenuProvider>
+        <Component {...pageProps} />
+      </MenuProvider>
+    </>
   )
 }
