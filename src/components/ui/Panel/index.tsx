50ad3d4740c4fb69e16b5b235fb3fb27f1674fd1
gotpop <liam@gotpop.net>
2022-11-14 02:59:29 +0000

    Adding brochure image component

---
+++ src/components/ui/Panel/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import styles from './Panel.module.css'
+
+export default function Panel({ image }) {
+  return (
+    <section className={styles.panel}>
+      <div className={styles.content}>
+        <h3>Panel</h3>
+      </div>
+      <Image className={styles.image} src={image} alt="Picture of the author" />
+    </section>
+  )
+}
--- src/pages/brochure.tsx
+++ src/pages/brochure.tsx
@@ -1,12 +1,18 @@
 import LayoutStandard from '@layouts/LayoutStandard'
 import Meta from '@head/Meta'
 import { NextPage } from 'next'
+import Panel from '@components/ui/Panel'
+import booksPic from '@images/books.png'
+import htmlPic from '@images/html.png'
+import keyboardPic from '@images/keyboard.png'
+import macPic from '@images/mac.png'
 
 const Brochure: NextPage = () => {
   return (
     <LayoutStandard>
       <Meta />
       <h1>Brochure</h1>
+      <Panel image={booksPic} />
     </LayoutStandard>
   )
 }
