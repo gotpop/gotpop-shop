174a540f8ebb63bad009d341f89aa7baefa78c7b
gotpop <liam@gotpop.net>
2022-11-15 02:25:59 +0000

    Brochure page header val remove comments

--- src/pages/brochure.tsx
+++ src/pages/brochure.tsx
@@ -40,8 +40,6 @@ export const getStaticProps: GetStaticProps = async () => {
     method: 'GET',
     headers: {
       'User-Agent': '*',
-      // 'User-Agent':
-      //   'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
       Accept: 'application/json; charset=UTF-8'
     }
   })
