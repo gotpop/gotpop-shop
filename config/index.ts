359fae0718bc73fda4d9fad6394bc4d613884dc2
gotpop <liam@gotpop.net>
2022-11-16 18:55:31 +0000

    Debugging test

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,9 @@
-const dev = process.env.NODE_ENV !== 'production'
-const local = process.env.HOST
-const prod = process.env.PRODUCTION
+// const dev = process.env.NODE_ENV !== 'production'
+// const local = process.env.HOST
+// const prod = process.env.PRODUCTION
 
-export const server = dev ? local : prod
+// export const server = dev ? local : prod
+
+const dev = process.env.NODE_ENV !== 'production';
+
+export const server = dev ? 'http://localhost:3000' : 'https://gotpop-starter.vercel.app';
