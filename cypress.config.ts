a879cd21dc03b71cbb6db69ea5fe45b1266e3817
gotpop <liam@gotpop.net>
2022-11-16 20:22:29 +0000

    Retries in CY config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  "retries": {
+    "runMode": 2,
+    "openMode": 2
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
