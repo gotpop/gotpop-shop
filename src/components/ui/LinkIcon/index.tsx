f89cc816b638faf76ad9522828e234f4211aa68f
gotpop <liam@gotpop.net>
2022-11-16 16:37:59 +0000

    Fixing build error

--- src/components/ui/LinkIcon/index.tsx
+++ src/components/ui/LinkIcon/index.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link'
 import styles from './LinkIcon.module.css'
 
 const LinkIcon = ({ icon, children, href, vars }: ILinkIcon) => {
-  const Icon = icon
-
   return (
     <Link className={styles.link} href={href} style={vars}>
-      <span>{children}</span>
-      <Icon />
+      <>
+        <span>{children}</span>
+        {icon}
+      </>
     </Link>
   )
 }
@@ -17,7 +17,7 @@ const LinkIcon = ({ icon, children, href, vars }: ILinkIcon) => {
 LinkIcon.defaultProps = {
   children: 'Visit',
   href: '/',
-  icon: <AiOutlineLink />,
+  icon: null,
   vars: {}
 }
 
