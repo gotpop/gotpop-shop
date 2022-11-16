e5c9b864bf8d3cc96091cfc3b257ba2af48d972b
gotpop <liam@gotpop.net>
2022-11-16 15:57:13 +0000

    Removing props logic

--- src/components/ui/Logo/index.tsx
+++ src/components/ui/Logo/index.tsx
@@ -1,21 +1,11 @@
-import { useEffect, useRef } from 'react'
-
 import { GiSpiralLollipop } from 'react-icons/gi'
 import Link from 'next/link'
 import styles from './Logo.module.css'
 
-const Logo = ({ properties }) => {
-  const h1Ref = useRef(null)
-
-  useEffect(() => {
-    properties?.forEach(prop => {
-      h1Ref.current?.style.setProperty(prop.var, prop.value)
-    })
-  }, [properties])
-
+const Logo = ({ vars }) => {
   return (
     <Link className={styles.logo} href="/">
-      <h1 ref={h1Ref}>
+      <h1>
         <GiSpiralLollipop />
         <span>Starter</span>
       </h1>
@@ -24,7 +14,7 @@ const Logo = ({ properties }) => {
 }
 
 Logo.defaultProps = {
-  properties: null
+  vars: null
 }
 
 export default Logo
