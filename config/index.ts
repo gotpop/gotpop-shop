55eabfe210be2213cda83d82998252fb51bf7e72
gotpop <liam@gotpop.net>
2022-11-16 20:39:43 +0000

    Updating config

--- config/index.ts
+++ config/index.ts
@@ -1,9 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production'
-const local = process.env.LOCALHOST
-const prod = process.env.PRODUCTION
+const local = 'http://localhost:3000'
+const prod = 'https://gotpop-starter.vercel.app'
 
 export const server = dev ? local : prod
-
-
-
-// export const server = dev ? 'http://localhost:3000' : 'https://gotpop-starter.vercel.app';
