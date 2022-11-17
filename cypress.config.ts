5920af26ef4de6541430378c5f30b17bd5ac98b6
gotpop <liam@gotpop.net>
2022-11-17 00:24:08 +0000

    Cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  "retries": {
-    "runMode": 2,
-    "openMode": 2
+  retries: {
+    runMode: 2,
+    openMode: 2,
   },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
     },
   },
 });
