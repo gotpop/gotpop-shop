f4d36c2ba272daffd483bf05ef5235d2df65cfc2
gotpop <liam@gotpop.net>
2022-11-18 15:24:44 +0000

    Editing Cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,17 +5,17 @@ export default defineConfig({
     runMode: 2,
     openMode: 2,
   },
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-
+  video: false,
+  screenshotOnRunFailure: false,
   component: {
     devServer: {
       framework: "next",
       bundler: "webpack",
     },
   },
-});
+})
