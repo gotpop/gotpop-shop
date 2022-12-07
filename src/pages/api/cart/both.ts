899e15a0bec235aaaec8922a25c673d1f310bada
GotPop <liam@gotpop.net>
2022-12-07 21:55:43 +0000

    `Enhancement:` Move to SWR fetch/server auth model

---
+++ src/pages/api/cart/both.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import prisma from '@lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { body } = req
+  const { id, quantity } = body
+
+  console.log('id, quantity :', id, quantity);
+
+  const currentUser = await prisma.user.findUnique({
+    where: { email: 'alice@prisma.io' },
+    include: {
+      Carts: {
+        include: {
+          CartItems: true
+        }
+      }
+    }
+  })
+
+  // const updateQuantity = 
+  // const dontUpdateQuantity = {}
+
+  // const makeCartItem = await prisma.cartItem.upsert({
+  //   where: { productId: id },
+  //   update: { quantity: quantity },
+  //   create: {
+  //     product: {
+  //       connect: { id: id },
+  //     },
+  //     Cart: {
+  //       connect: { id: currentUser.Carts[0].id },
+  //     },
+  //   },
+  // })
+
+  // return res.status(200).json(makeCartItem)
+  return res.status(200).json(currentUser)
+}
