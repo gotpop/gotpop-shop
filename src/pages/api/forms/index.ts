b522483d27c3d0e95a3c80c8c298ab1b6089dab6
GotPop <liam@gotpop.net>
2022-12-09 15:14:21 +0000

    `Task:` Adding account pages

--- src/pages/api/forms/index.ts
+++ src/pages/api/forms/index.ts
@@ -1,11 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 import { IForm } from '@types'
-import { forms } from '@data/forms'
 
 export default function handler(
   _req: NextApiRequest,
   res: NextApiResponse<IForm[]>
 ) {
-  return res.status(200).json(forms)
+  // return res.status(200).json(forms)
 }
