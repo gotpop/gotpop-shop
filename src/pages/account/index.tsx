ab8a324dcf4c9edd61e7ab54f9899393bcb072a7
GotPop <gotpop@users.noreply.github.com>
2023-02-01 17:05:31 +0000

    `Enhancement:` Account content

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -9,14 +9,14 @@ const Account: NextPage = () => {
         <Meta />
         <article>
         <h2>Account</h2>
-        <p>This application is a 'living demo'. The first stage was to have a raw CSS front end performing CRUD operations to a PostgreSQL database.</p>
+        <p>This application is a &apos;living demo&apos;. The first stage was to have a raw CSS front end performing CRUD operations to a PostgreSQL database.</p>
 
-        <p>However as I'm sure you can see there's not yet any concept of users or login. So as a next step I plan to use next-auth.js to autheticate users, which means I will then be able to build out things like a user profile and saved carts etc.</p>
+        <p>However as I&apos;m sure you can see there&apos;s not yet any concept of users or login. So as a next step I plan to use next-auth.js to autheticate users, which means I will then be able to build out things like a user profile and saved carts etc.</p>
 
-        <p>In addition to this I'd like to add:</p>
+        <p>In addition to this I&apos;d like to add:</p>
         <ul>
           <li>More testing coverage</li>
-          <li>Server > client type saftey with Trpc</li>
+          <li>Server to client type saftey with Trpc</li>
           <li>Large dataset of dummy products to enable searching etc</li>
           <li>Continued performance, browser testing & UX improvments</li>
         </ul>
