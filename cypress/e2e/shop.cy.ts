5f32197b41e1a7bfe5bc89ce0b980333cee3bdab
GotPop <gotpop@users.noreply.github.com>
2023-02-02 19:25:45 +0000

    `Enhancement:` Adding e2e tests

--- cypress/e2e/shop.cy.ts
+++ cypress/e2e/shop.cy.ts
@@ -1,22 +1,7 @@
 describe('Shop', () => {
     it('Should navigate to the shop page & add item to cart', () => {
         cy.visit('/')
-
-        cy.intercept("GET", "/api/cart/clc3riz4k000spzqj2g5gh2ce", {
-            fixture: "product-1.json",
-        }).as("mockedProduct1")
-
-        cy.intercept("GET", "/api/cart/clc3rj01o000ypzqj4vukpps0", {
-            fixture: "product-2.json",
-        }).as("mockedProduct2")
-
-        cy.intercept("GET", "/api/cart/clc3rj0nx0014pzqj3mso1sev", {
-            fixture: "product-3.json",
-        }).as("mockedProduct3")
-
-        cy.intercept("GET", "/api/cart/clc3rj19z001apzqjmvmk145x", {
-            fixture: "product-4.json",
-        }).as("mockedProduct4")
+        cy.products()
 
         cy.get('[data-test="shop"]').click()
         cy.url().should('include', 'shop')
@@ -34,6 +19,7 @@ describe('Shop', () => {
 
         cy.contains('HTML & JS').should('be.visible') 
         cy.contains('£10.99').should('be.visible') 
+        cy.get('[data-cy="button-cart-close"]').click()
     })
 })
 
